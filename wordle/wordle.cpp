2f6c01d937d9dac20aca1927b362efb72f8dad9c
angelptli <ali95@mail.ccsf.edu>
2022-02-07 09:45:40 -0800

    Add documentation for function methods

--- wordle/wordle.cpp
+++ wordle/wordle.cpp
@@ -8,6 +8,17 @@ using std::vector;
 using std::cout;
 using std::cin;
 
+/**
+ * Fill a vector with indices that locate matching green letters.
+ * 
+ * Compare target and user guess letters. A match will prompt an
+ * add of their shared index number to the vector. A non-match adds
+ * a "404" placeholder to indicate there is no match.
+ * 
+ * @param target A random target word that user needs to guess.
+ * @param word The user's guess taken from their input.
+ * @return vector<int> a vector that stores indices of int type.
+ */
 vector<int> getGreenIndices(string target, string word)
 {
     vector<int> indexTracker;
@@ -23,6 +34,15 @@ vector<int> getGreenIndices(string target, string word)
     return indexTracker;
 }
 
+/**
+ * Determine the number of green matching letters.
+ * 
+ * Count the number of indices in a given vector of indices.
+ * The count increments when an index is not a "404" placeholder.
+ * 
+ * @param indexTracker A vector of indices of int type.
+ * @return int count of green matching letters.
+ */
 int getNumOfGreen(vector<int> indexTracker)
 {
     int matchCount = 0;
@@ -36,6 +56,12 @@ int getNumOfGreen(vector<int> indexTracker)
     return matchCount;
 }
 
+/**
+ * Fill a vector with 5-letter words from a word text file that contain
+ * a 5-letter word on each line.
+ * 
+ * @return vector<string> a vector that stores strings.
+ */
 vector<string> getWordFileArray()
 {
     ifstream fin("words.txt");
@@ -48,6 +74,16 @@ vector<string> getWordFileArray()
     return words;
 }
 
+/**
+ * Get a random target word for the user to guess.
+ * 
+ * Seed a random number generator and use this number as an index
+ * to get the corresponding word from a vector filled with 5-letter
+ * words.
+ * 
+ * @param words A vector filled with 5-letter words.
+ * @return string randomly generated target word for user to guess. 
+ */
 string getRandomTarget(vector<string> words)
 {
     const int MIN_VALUE = 1;
@@ -60,6 +96,21 @@ string getRandomTarget(vector<string> words)
     return target;
 }
 
+/**
+ * Return a vector filled with words that shares at least one green
+ * letter with the target word.
+ * 
+ * A green letter indicates the letter is found in the target word and
+ * is in the same index place as the target word.
+ * 
+ * @param words A vector filled with 5-letter words.
+ * @param size The number of elements in the vector.
+ * @param indexVector A vector filled with indices that represent the
+ *      indices of green letters.
+ * @param target A random target word that user needs to guess.
+ * @return vector<string> a vector filled with words that contain at
+ *      least one green letter in common with `target`.
+ */
 vector<string> getGreenWords(vector<string> words, int size, vector<int> indexVector, string target)
 {
     vector<string> greenWords;
@@ -86,6 +137,21 @@ vector<string> getGreenWords(vector<string> words, int size, vector<int> indexVe
     return greenWords;
 }
 
+/**
+ * Return a vector filled with words that contains at least one yellow
+ * letter.
+ * 
+ * A yellow letter indicates the letter is found in the target word but
+ * is not in the correct index place as the target word.
+ * 
+ * @param words A vector filled with 5-letter words.
+ * @param size The number of elements in the vector.
+ * @param indexVector A vector filled with indices that represent the
+ *      indices of green letters.
+ * @param target A random target word that user needs to guess.
+ * @return vector<string> a vector filled with words that contain at
+ *      least one yellow letter.
+ */
 vector<string> getYellowWords(vector<string> words, int size, vector<int> indexVector, string target)
 {
     vector<string> yellowWords;
@@ -108,6 +174,12 @@ vector<string> getYellowWords(vector<string> words, int size, vector<int> indexV
     return yellowWords;
 }
 
+/**
+ * Print a given vector's elements onto the terminal.
+ * 
+ * @param strVector A vector filled with 5-letter words.
+ * @param size The number of elements in the vector.
+ */
 void printVector(vector<string> strVector, int size)
 {
     for (int i = 0; i < size; i++)
@@ -119,6 +191,19 @@ void printVector(vector<string> strVector, int size)
     }
 }
 
+/**
+ * Get the color code of the user's guess for comparing the target
+ * and user's guess.
+ * 
+ * The color codes are
+ * G (green): same letter with same index place
+ * Y (yellow): letter contained in target but not the same index place
+ * - (grey): letter not found in target
+ * 
+ * @param target A random target word that user needs to guess.
+ * @param userGuess The user's guess for one round.
+ * @return string contains color code for target and guess comparison.
+ */
 string convertGuessToColors(string target, string userGuess)
 {
     string colorString;
@@ -138,16 +223,17 @@ string convertGuessToColors(string target, string userGuess)
 
 int main()
 {
-    int guessCount = 0;
-    int greenLetters;
-    string userGuess;
+    int guessCount = 0;                         // Tracks number of guesses
+    int greenLetters;                           // Matching letters & same index
+    string userGuess;                           // User guess from input
 
-    vector<string> words = getWordFileArray();
-    vector<string> matchingWordVector;
-    string target = getRandomTarget(words);
+    vector<string> words = getWordFileArray();  // 5-letter word dictionary
+    vector<string> matchingWordVector;          // Stores green or yellow words
+    string target = getRandomTarget(words);     // Target to guess
 
     cout << "Welcome to Wordle Simulator\n"
          << "---------------------------\n\n";
+         
     while (guessCount != 6 && greenLetters != 5)
     {
         greenLetters = 0;
